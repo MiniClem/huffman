2328bfce1345eeb6844d45987c5174aeb9e1f4dc
Miniclem <cmonesti@gmail.com>
2019-12-03 10:33:52 +0100

    Ajout des fichiers src de base de l'algo

---
+++ include/arbre.h
@@ -0,0 +1,28 @@
+#ifndef __FILE_H_
+#define __FILE_H_
+#include <stdio.h>
+#include "liste_ext.h"
+
+typedef struct noeud
+{
+	Elt elt;
+	struct noeud *fils_gauche;
+	struct noeud *fils_droit;
+} Noeud;
+
+typedef struct noeud *Arbre;
+
+Arbre creer_arbre(Elt x, Arbre fg, Arbre fd);
+Arbre detruire_arbre(Arbre a);
+
+int est_arbre_vide(Arbre a);
+
+void print_racine(Arbre a);
+void prof_prefixe(Arbre a, void action(Arbre));
+
+void largeur(Arbre a, void action(Arbre));
+int hauteur(Arbre a);
+
+int nb_feuille(Arbre a);
+Noeud *rechercher(Arbre a, Elt e);
+#endif
---
+++ include/liste.h
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <assert.h>
+
+// Poiteur sur quelque chose
+typedef void *Elt;
+
+struct Maillon
+{
+    Elt elt;
+    struct Maillon *suiv;
+};
+
+typedef struct Maillon Maillon;
+typedef Maillon *Liste;
+
+// Création
+Liste cree_liste_vide();
+
+// Observateur
+int est_liste_vide(Liste l);
+
+// Opération
+Liste ajout_liste(Liste l, Elt e);
+
+Elt tete_liste(Liste l);
+
+Liste queue_liste(Liste l);
+
+Liste liberer_liste(Liste l);
+
+Liste liberer_maillon(Maillon *m);
