4ffc8d513dfe58ffc8cac88afece19243ece15dc
Miniclem <cmonesti@gmail.com>
2019-12-05 21:24:57 +0100

    format

--- src/file.c
+++ src/file.c
@@ -1,58 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char* lire_caractere_fichier(FILE* file){
+char *lire_caractere_fichier(FILE *file)
+{
+	int i = 0;
+	while (fgetc(file) != EOF)
+	{
+		i++;
+	}
 
-int i= 0;
-	while (fgetc(file)!= EOF)
-        {
-            i++;
-        }  //
-	
-    char* caracterelu=(char*)malloc(i*sizeof(char));
-	printf("%d",i);
+	char *caracterelu = (char *)malloc(i * sizeof(char));
+	printf("%d", i);
 	rewind(file);
-	int j=i;
-	i=0;
-        // Boucle de lecture des caractères un à un
-        while ( i<j )
-        {
-            caracterelu[i] = (char)fgetc(file); // On lit le caractère
+	int j = i;
+	i = 0;
+	// Boucle de lecture des caractères un à un
+	while (i < j)
+	{
+		caracterelu[i] = (char)fgetc(file); // On lit le caractère
 
-			i++;
-        }  // On continue tant que fgetc n'a pas retourné EOF (fin de file)
-		
-		caracterelu[i+1] ='\0';
-        fclose(file);
-    
+		i++;
+	} // On continue tant que fgetc n'a pas retourné EOF (fin de file)
 
-    return caracterelu;
+	caracterelu[i + 1] = '\0';
+	fclose(file);
+
+	return caracterelu;
 }
 
-char* char_to_byte(char char_to_convert){
-	char* str =(char*)malloc(8*sizeof(char));
-	int a=128;
-	int i=0;
+char *char_to_byte(char char_to_convert)
+{
+	char *str = (char *)malloc(8 * sizeof(char));
+	int a = 128;
+	int i = 0;
 
-	
-	for(i=0;i<8;i++){
-	if( ((int)char_to_convert&a) == 0){
-			str[i]='0';
-	}
-	else{
-		str[i]='1';
-	}
+	for (i = 0; i < 8; i++)
+	{
+		if (((int)char_to_convert & a) == 0)
+		{
+			str[i] = '0';
+		}
+		else
+		{
+			str[i] = '1';
+		}
 
-	a=a/2;
+		a = a / 2;
 	}
 
 	return str;
-
-} 
-	
+}
 
 int main()
-{	char c ='A';
+{
+	char c = 'A';
 	/*FILE* fichier = NULL;
 	fichier = fopen("test.txt", "r");
 	if (fichier != NULL)
@@ -65,7 +66,7 @@ int main()
         printf("Impossible d'ouvrir le file test.txt");
     }
 	*/
-	char* test = char_to_byte(c);
-	printf("%s",test);
+	char *test = char_to_byte(c);
+	printf("%s", test);
 	return 0;
 }
