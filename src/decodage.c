94bb56ba6aa740b8926f2d82a7f5d2e11404f9aa
Julien <julien.raige@gmail.com>
2019-12-06 09:58:03 +0100

    Decodage KO
    KO

--- src/decodage.c
+++ src/decodage.c
@@ -4,21 +4,21 @@
 
 Arbre lire_dico(char* code)
 {
+	//utiliser fonction créer_arbre_vide
+	//utiliser fonction association fils_gauche et fils_droit
 	static int compteur=0;
-	Arbre Pere;
-	Arbre filsgauche;
-	Arbre filsdroit;
-	if (code[0]=='0')
+	Arbre Pere = (Noeud *) malloc(sizeof(Noeud));
+	Arbre filsgauche = (Noeud *) malloc(sizeof(Noeud)) ;
+	Arbre filsdroit = (Noeud *) malloc(sizeof(Noeud)) ;
+	if (*(code+compteur)=='0')
 	{
-		filsgauche = lire_dico((&(code[compteur +1])));
+		filsgauche = lire_dico(code+(++compteur));
 		
-		filsdroit = lire_dico(code[compteur +1]);
+		filsdroit = lire_dico(code+(++compteur));
 		Pere = creer_arbre('0',0,filsgauche,filsdroit);
 
 	}else{
-		
-		
-		Pere = creer_arbre(lire_ascii(code[compteur +1]),0,NULL,NULL);
+		Pere = creer_arbre(lire_ascii(code+compteur),0,NULL,NULL);
 	}
 	return Pere;
 }
@@ -43,9 +43,9 @@ Elt lire_ascii(char* octet)
 
 // TESTS
 int main(){
-	char* code = "01100000111000010";
+	char* code = "0010100000110100001110110010100101101001101100111";
+	//char* ascii = "01000001" ;
 	Arbre d = lire_dico(code);
-
 	
 	return  0;
 }
