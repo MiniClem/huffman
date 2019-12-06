a273d342320709ebd1e3826ab942340283b852f4
Miniclem <cmonesti@gmail.com>
2019-12-06 09:19:09 +0100

    fix

--- src/file.c
+++ src/file.c
@@ -1,36 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef char byte;
+
 char *lire_caractere_fichier(FILE *file)
 {
-	int i = 0;
-	while (fgetc(file) != EOF)
-	{
-		i++;
-	}
+	long size;
 
-	char *caracterelu = (char *)malloc(i * sizeof(char));
-	printf("%d", i);
+	fseek(file, 0L, SEEK_END);
+	size = ftell(file);
+
+	char *caracterelu = (char *)malloc((size + 1) * sizeof(char));
 	rewind(file);
-	int j = i;
-	i = 0;
+
 	// Boucle de lecture des caractères un à un
-	while (i < j)
+	for (int i = 0; i < size; i++)
 	{
 		caracterelu[i] = (char)fgetc(file); // On lit le caractère
+	}										// On continue tant que fgetc n'a pas retourné EOF (fin de file)
+	caracterelu[size] = '\0';
 
-		i++;
-	} // On continue tant que fgetc n'a pas retourné EOF (fin de file)
-
-	caracterelu[i + 1] = '\0';
 	fclose(file);
-
 	return caracterelu;
 }
 
-char *char_to_byte(char char_to_convert)
+byte *char_to_byte(char char_to_convert)
 {
-	char *str = (char *)malloc(8 * sizeof(char));
+	byte *str = (byte *)malloc(8 * sizeof(byte));
 	int a = 128; // 0b10000000
 
 	for (int i = 0; i < 8; i++)
