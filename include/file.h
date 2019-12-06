6a5545845d4d646a3007592078c536e85ff57107
Rkd <kallabrayan@gmail.com>
2019-12-06 14:18:50 +0100

    byte to char

---
+++ include/file.h
@@ -0,0 +1,13 @@
+#ifndef __encodage_h__
+#define __encodage_h__
+#include "arbre.h"
+#include <string.h>
+#include <math.h>
+
+
+void ecrire_bit(FILE* file, char bit);
+void lire_bit(File* file);
+
+
+
+#endif
--- src/file.c
+++ src/file.c
@@ -46,9 +46,27 @@ byte *char_to_byte(char char_to_convert)
 	return str;
 }
 
+char *byte_to_char(byte* char_to_convert)
+{
+	char *str = (char *)calloc(1, sizeof(char));
+	int a = 128;
+
+	for (int i = 0; i < 8; i++)
+	{
+		if ( char_to_convert[i] == '1')
+		{
+			*str &= a;
+		}		
+	}
+	a/=2;
+
+	return str;
+}
+
 int main()
 {
-	char c = 'A';
+	//char c = 'A';
+	byte b[] = {0b00000000,0b01000000,0b00000000,0b00000000,0b00000000,0b00000000,0b00000000,0b00000001};
 	/*FILE* fichier = NULL;
 	fichier = fopen("test.txt", "r");
 	if (fichier != NULL)
@@ -61,7 +79,7 @@ int main()
         printf("Impossible d'ouvrir le file test.txt");
     }
 	*/
-	char *test = char_to_byte(c);
-	printf("%s", test);
+	char* test = byte_to_char(b);
+	printf("%c", test[0]);
 	return 0;
 }
